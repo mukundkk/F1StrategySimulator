caa7d9a2d1ecbedffd604b3690a4c542f71c6892
mukundkk <mukund.krishnakumar@gmail.com>
2021-02-16 11:10:09 -0800

    Add standard deviation calculation for DNFs

--- src/Models/DNFModel.java
+++ src/Models/DNFModel.java
@@ -2,8 +2,10 @@ package Models;
 
 import Data.GlobalInfo;
 import FileIO.ResultsReader;
+import org.apache.commons.math3.analysis.function.Sqrt;
 import org.apache.commons.math3.distribution.BetaDistribution;
 import org.apache.commons.math3.distribution.BinomialDistribution;
+import org.apache.commons.math3.stat.descriptive.summary.Sum;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -19,7 +21,7 @@ public class DNFModel {
 	public DNFModel() {
 		driverList = GlobalInfo.getDriverList();
 		calcAvgTotalDNF();
-		System.out.println(avgDNFFraction);
+		calcStdDev();
 	}
 
 	private void calcAvgTotalDNF() {
@@ -52,5 +54,25 @@ public class DNFModel {
 
 	private boolean didDriverDNF(String circuit, String lastName) {
 		return new ResultsReader(circuit, true).didDNF(lastName);
+	}
+
+	private void calcStdDev() {
+		// use average dnf fraction of all drivers as mean
+		double mean = avgDNFFraction;
+		double[] terms = new double[driverList.size()];
+
+		// for each driver, calculate squared difference between driver DNF fraction and mean - used as term in sum
+		for (int i = 0; i < driverList.size(); i++) {
+			double driverDNFFrac = calcAvgDriverDNF(driverList.get(i).getLastName());
+			double term = Math.pow((driverDNFFrac - mean), 2);
+			terms[i] = term;
+		}
+
+		// find the mean of squared differences
+		double sum = new Sum().evaluate(terms, 0, terms.length);
+		double meanOfSum = sum / driverList.size();
+
+		// standard deviation is the square root of that mean
+		stdDevDNF = Math.sqrt(meanOfSum);
 	}
 }
